5bca4e3bf94531148222c994bc83dbaf0fea1c0c
EC2 Default User <violet794.py16@gmail.com>
2021-04-04 23:14:32 +0000

    [add]3-7

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,6 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'b814ba31974d2964eb4b8d984f2c783165dfd74fd2bb4e33d432063109e21b0208714f9fad1552655985d2f1cccee5919c7bed062ae5af30d6fae63114d6b7f1'
+
